f369967fd5f765ae80c933aaf18c10eed3c2fb2b
Edgar <edgar.gibert@qustodio.com>
2020-11-13 12:00:55 +0100

    test 2

--- src/steps/when.js
+++ src/steps/when.js
@@ -103,3 +103,4 @@ When(/^I refresh the current page$/, refresh);
 When(/^I open the created profile with the inputfield "([^"]*)?"$/, selectCreatedProfile);
 
 When(/^When I scroll to the element "([^"]*)?"$/, selectCreatedProfile);
+sadsdsd

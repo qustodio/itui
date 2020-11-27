218d99b73440efcf85ec45521fdfa578191b45ec
Edgar <edgar.gibert@qustodio.com>
2020-11-27 13:39:36 +0100

    QA-1119 Add activation code flows

--- src/steps/given.js
+++ src/steps/given.js
@@ -1,4 +1,6 @@
-import  { Given } from 'cucumber';
+import {
+  Given
+} from 'cucumber';
 
 import checkContainsAnyText from '../support/check/checkContainsAnyText';
 import checkIsEmpty from '../support/check/checkIsEmpty';
@@ -21,115 +23,120 @@ import isVisible from '../support/check/isVisible';
 import openWebsite from '../support/action/openWebsite';
 import loginFP from '../support/action/loginFP';
 import resizeScreenSize from '../support/action/resizeScreenSize';
+import apiRequest from '../support/action/apiRequest';
 
 
-    Given(
-        /^I open the (url|site) "([^"]*)?"$/,
-        openWebsite
-    );
+Given(
+  /^I open the (url|site) "([^"]*)?"$/,
+  openWebsite
+);
 
-    Given(
-        /^the element "([^"]*)?" is( not)* visible$/,
-        isVisible
-    );
+Given(
+  /^the element "([^"]*)?" is( not)* visible$/,
+  isVisible
+);
 
-    Given(
-        /^the element "([^"]*)?" is( not)* enabled$/,
-        isEnabled
-    );
+Given(
+  /^the element "([^"]*)?" is( not)* enabled$/,
+  isEnabled
+);
 
-    Given(
-        /^the element "([^"]*)?" is( not)* selected$/,
-        checkSelected
-    );
+Given(
+  /^the element "([^"]*)?" is( not)* selected$/,
+  checkSelected
+);
 
-    Given(
-        /^the checkbox "([^"]*)?" is( not)* checked$/,
-        checkSelected
-    );
+Given(
+  /^the checkbox "([^"]*)?" is( not)* checked$/,
+  checkSelected
+);
 
-    Given(
-        /^there is (an|no) element "([^"]*)?" on the page$/,
-        checkElementExists
-    );
+Given(
+  /^there is (an|no) element "([^"]*)?" on the page$/,
+  checkElementExists
+);
 
-    Given(
-        /^the title is( not)* "([^"]*)?"$/,
-        checkTitle
-    );
+Given(
+  /^the title is( not)* "([^"]*)?"$/,
+  checkTitle
+);
 
-    Given(
-        /^the element "([^"]*)?" contains( not)* the same text as element "([^"]*)?"$/,
-        compareText
-    );
+Given(
+  /^the element "([^"]*)?" contains( not)* the same text as element "([^"]*)?"$/,
+  compareText
+);
 
-    Given(
-        /^I login to FP with my test account$/,
-        loginFP
-    );
+Given(
+  /^I login to FP with my test account$/,
+  loginFP
+);
 
-    Given(
-        /^the (button|element) "([^"]*)?"( not)* matches the text "([^"]*)?"$/,
-        checkEqualsText
-    );
+Given(
+  /^the (button|element) "([^"]*)?"( not)* matches the text "([^"]*)?"$/,
+  checkEqualsText
+);
 
-    Given(
-        /^the (button|element) "([^"]*)?"( not)* contains the text "([^"]*)?"$/,
-        checkContainsText
-    );
+Given(
+  /^the (button|element) "([^"]*)?"( not)* contains the text "([^"]*)?"$/,
+  checkContainsText
+);
 
-    Given(
-        /^the (button|element) "([^"]*)?"( not)* contains any text$/,
-        checkContainsAnyText
-    );
+Given(
+  /^the (button|element) "([^"]*)?"( not)* contains any text$/,
+  checkContainsAnyText
+);
 
-    Given(
-        /^the (button|element) "([^"]*)?" is( not)* empty$/,
-        checkIsEmpty
-    );
+Given(
+  /^the (button|element) "([^"]*)?" is( not)* empty$/,
+  checkIsEmpty
+);
 
-    Given(
-        /^the page url is( not)* "([^"]*)?"$/,
-        checkUrl
-    );
+Given(
+  /^the page url is( not)* "([^"]*)?"$/,
+  checkUrl
+);
 
-    Given(
-        /^the( css)* attribute "([^"]*)?" from element "([^"]*)?" is( not)* "([^"]*)?"$/,
-        checkProperty
-    );
+Given(
+  /^the( css)* attribute "([^"]*)?" from element "([^"]*)?" is( not)* "([^"]*)?"$/,
+  checkProperty
+);
 
-    Given(
-        /^the cookie "([^"]*)?" contains( not)* the value "([^"]*)?"$/,
-        checkCookieContent
-    );
+Given(
+  /^the cookie "([^"]*)?" contains( not)* the value "([^"]*)?"$/,
+  checkCookieContent
+);
 
-    Given(
-        /^the cookie "([^"]*)?" does( not)* exist$/,
-        checkCookieExists
-    );
+Given(
+  /^the cookie "([^"]*)?" does( not)* exist$/,
+  checkCookieExists
+);
 
-    Given(
-        /^the element "([^"]*)?" is( not)* ([\d]+)px (broad|tall)$/,
-        checkDimension
-    );
+Given(
+  /^the element "([^"]*)?" is( not)* ([\d]+)px (broad|tall)$/,
+  checkDimension
+);
 
-    Given(
-        /^the element "([^"]*)?" is( not)* positioned at ([\d]+)px on the (x|y) axis$/,
-        checkOffset
-    );
+Given(
+  /^the element "([^"]*)?" is( not)* positioned at ([\d]+)px on the (x|y) axis$/,
+  checkOffset
+);
 
-    Given(
-        /^I have a screen that is ([\d]+) by ([\d]+) pixels$/,
-        resizeScreenSize
-    );
+Given(
+  /^I have a screen that is ([\d]+) by ([\d]+) pixels$/,
+  resizeScreenSize
+);
 
-    Given(
-        /^I have closed all but the first (window|tab)$/,
-        closeAllButFirstTab
-    );
+Given(
+  /^I have closed all but the first (window|tab)$/,
+  closeAllButFirstTab
+);
 
-    Given(
-        /^a (alertbox|confirmbox|prompt) is( not)* opened$/,
-        checkModal
-    );
+Given(
+  /^a (alertbox|confirmbox|prompt) is( not)* opened$/,
+  checkModal
+);
 
+Given(
+  /^api request$/,
+  apiRequest
+);

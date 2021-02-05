01c74445be5b9957e7ecfeebba9ccb13c2ea90cf
David Navas2 <davidnavas2@MacBook-Pro-de-David.local>
2021-02-05 18:03:45 +0100

    QA-1143 add index.js for support module

---
+++ src/support/index.js
@@ -0,0 +1,57 @@
+
+// actions
+
+export {default as clearInputField} from './action/clearInputField';
+export {default as clickElement} from './action/clickElement';
+export {default as closeAllButFirstTab} from './action/closeAllButFirstTab';
+export {default as closeLastOpenedWindow} from './action/closeLastOpenedWindow';
+export {default as deleteCookies} from './action/deleteCookies';
+export {default as dragElement} from './action/dragElement';
+export {default as focusLastOpenedWindow} from './action/focusLastOpenedWindow';
+export {default as handleModal} from './action/handleModal';
+export {default as moveTo} from './action/moveTo';
+export {default as openWebsite} from './action/openWebsite';
+export {default as pause} from './action/pause';
+export {default as pressButton} from './action/pressButton';
+export {default as scroll} from './action/scroll';
+export {default as selectOption} from './action/selectOption';
+export {default as selectOptionByIndex} from './action/selectOptionByIndex';
+export {default as setCookie} from './action/setCookie';
+export {default as setInputField} from './action/setInputField';
+export {default as setPromptText} from './action/setPromptText';
+export {default as setRandomEmailInputField} from './action/setRandomEmailInputField';
+export {default as setRandomNameInputField} from './action/setRandomNameInputField';
+export {default as setWindowSize} from './action/setWindowSize';
+export {default as waitFor} from './action/waitFor';
+export {default as waitForDisplayed} from './action/waitForDisplayed';
+
+//checks
+export {default as checkClass} from './check/checkClass';
+export {default as checkContainsAnyText} from './check/checkContainsAnyText';
+export {default as checkContainsText} from './check/checkContainsText';
+export {default as checkCookieContent} from './check/checkCookieContent';
+export {default as checkCookieExists} from './check/checkCookieExists';
+export {default as checkDimension} from './check/checkDimension';
+export {default as checkElementExists} from './check/checkElementExists';
+export {default as checkEqualsText} from './check/checkEqualsText';
+export {default as checkFocus} from './check/checkFocus';
+export {default as checkFontProperty} from './check/checkFontProperty';
+export {default as checkInURLPath} from './check/checkInURLPath';
+export {default as checkIsEmpty} from './check/checkIsEmpty';
+export {default as checkIsOpenedInNewWindow} from './check/checkIsOpenedInNewWindow';
+export {default as checkModal} from './check/checkModal';
+export {default as checkModalText} from './check/checkModalText';
+export {default as checkNewWindow} from './check/checkNewWindow';
+export {default as checkOffset} from './check/checkOffset';
+export {default as checkProperty} from './check/checkProperty';
+export {default as checkSelected} from './check/checkSelected';
+export {default as checkTitle} from './check/checkTitle';
+export {default as checkTitleContains} from './check/checkTitleContains';
+export {default as checkURL} from './check/checkURL';
+export {default as checkURLPath} from './check/checkURLPath';
+export {default as checkWithinViewport} from './check/checkWithinViewport';
+export {default as compareText} from './check/compareText';
+export {default as isDisplayed} from './check/isDisplayed';
+export {default as isEnabled} from './check/isEnabled';
+export {default as isExisting} from './check/isExisting';
+

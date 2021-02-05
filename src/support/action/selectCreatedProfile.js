666302e9e57bd6f6b333afe1f710e96529751c4f
David Navas2 <davidnavas2@MacBook-Pro-de-David.local>
2021-02-05 12:59:16 +0100

    QA-1141 delete methods moved to PAR

--- src/support/action/selectCreatedProfile.js
+++
@@ -1,9 +0,0 @@
-/**
- * Select profile by name
- * @param  {String}   selector inputField where the profile name is stored
- */
-module.exports = (selector) => {
-    const profileName = `//div[@class="ProfileListItem__name"][contains(., "${global.world[selector]}")]`;
-  
-    browser["click"](profileName);
-  };
